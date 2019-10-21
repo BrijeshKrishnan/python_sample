aac9573a60f619f2753bb9544611e21fe7db156d
Brijesk <brijeshk_raj@yahoo.com>
2019-10-21 14:39:00 +0530

    nlp_de-duplication/cosine.py modified to add comments and generic path

--- nlp_de-duplication/cosine.py
+++ nlp_de-duplication/cosine.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 # Reading an excel file using Python 
 import xlrd
 import sys
+import os
 WORD = re.compile(r'\w+')
 import xlsxwriter 
 # Give the location of the file 
@@ -46,17 +47,25 @@ class MyWindow:
         
 
     def browsefunc(self):
-      self.filename = filedialog.askopenfilename()
-      self.path_t.delete(0, END)
-      self.path_t.insert(0,str(self.filename))
-      return
+        """
+        Function used for providing the Browse to file path in the GUI
+        """
+        self.filename = filedialog.askopenfilename()
+        self.path_t.delete(0, END)
+        self.path_t.insert(0,str(self.filename))
+        return
+
     def validate_input(self):
+        """
+        Function used for validating whether all the mandatory input fields are provided or not
+        """
         if not (self.path_t.get() and self.sin_index_t.get() and 
-          self.tc_id_t.get() and self.ts_id_t.get()):#and 
-        #   self.te_id_t.get()):
-            print ("Enter values in field")
+          self.tc_id_t.get() and self.ts_id_t.get()):
             return
-    def submit(self):        
+    def submit(self):
+        """
+        Function which is the entry for all the processing activity. 
+        """        
         try:
             self.validate_input()
             self.process_file()
@@ -65,8 +74,24 @@ class MyWindow:
         except:
             print('Error')
 
-    def get_cosine(self, vec1, vec2):
+    def get_file_path(self):
+        """
+        Function used for getting the file path where the results can be stored
+        """
+        return str(os.path.dirname(self.path_t.get()))
     
+    def get_file_name(self):
+        """
+        Function used for getting the file name which can be used for naming the result
+        """
+        file_path = self.path_t.get().split("/")
+        return(os.path.splitext(file_path[-1])[0])
+
+    def get_cosine(self, vec1, vec2):
+        """
+        Function used for getting the cosine value
+        vec1, vec2: Input vector from the texts to be compared
+        """
         intersection = set(vec1.keys()) & set(vec2.keys())
         numerator = sum([vec1[x] * vec2[x] for x in intersection])
 
@@ -80,20 +105,36 @@ class MyWindow:
             return float(numerator) / denominator
 
     def text_to_vector(self, text):
+        """
+        Function used for converting the text to vector
+        text: Input text 
+        """
         words = WORD.findall(text)
         return Counter(words)
         
     def check_tolarance(self, actual_val=100, exp_val=100):
-        return abs(actual_val - exp_val) <= 1
+        """
+        Function used for checking the tolarance value (defaulted to 100)
+        actual_val: what is the actual value after processing the cosine
+        exp_val: what is the expected value for the cosine
+        """
+        if actual_val >= exp_val:
+            return abs(actual_val - exp_val) <= 1
+        else:
+            return 0
 
     def process_cosine(self):
-        file = open("C:\\SimilarityIndexProcessor\\SimilarityIndexProcessor.csv","w") 
-    # To open Workbook 
+        """
+        Function which process the cosine from the merged input data and generate a recomendation after processing
+        """
+        file = open(os.path.join(self.get_file_path(),self.get_file_name()+"_recomendation_%s.csv"%self.sin_index_t.get()),"w")
+        print(os.path.join(self.get_file_path(),self.get_file_name()+"_recomendation_%s.csv"%self.sin_index_t.get()))
+        # To open Workbook 
         if (int(self.sin_index_t.get()) >= 98):
              file.write("Testcase ID,Duplicates\n")
         else:
             file.write("Testcase ID,Potential Merge\n")
-        wb = xlrd.open_workbook("C:\\SimilarityIndexProcessor\\temp.xlsx") 
+        wb = xlrd.open_workbook(os.path.join(self.get_file_path(),self.get_file_name()+"_merged.xlsx"))
 
         sheet = wb.sheet_by_index(0) 
         for i in range(1, sheet.nrows):
@@ -109,9 +150,17 @@ class MyWindow:
                     file.write(str(test_case_id)+","+str(sheet.cell_value(j, 0))+"\n")
 
     def get_tc_id(self, row_num, sheet):
+        """
+        Function used for getting the testcase id from the input sheet
+        row_num : row number on which testcase id need to be fetched
+        steet : sheet of interest
+        """
         return str(sheet.cell_value(row_num, int(self.tc_id_t.get())))
     def create_temp_file(self):
-        workbook = xlsxwriter.Workbook("C:\\SimilarityIndexProcessor\\temp.xlsx") 
+        """
+        Function used for creating the temporary file which has just 2 columns 1: testcase id,2: all interested cells merged to form the steps
+        """
+        workbook = xlsxwriter.Workbook(os.path.join(self.get_file_path(),self.get_file_name()+"_merged.xlsx")) 
         worksheet = workbook.add_worksheet() 
         xls_row = 1
         worksheet.write(0, 0, "Testcase ID") 
@@ -144,6 +193,10 @@ class MyWindow:
         workbook.close()
 
     def process_file(self):
+        """
+        Function used for identifying the cells to be merged and create a dictionary which will help the further processing during the creating of 
+        consolidated steps in create_temp_file()
+        """
         wb = xlrd.open_workbook(self.path_t.get())
         sheet = wb.sheet_by_index(0)
         jump_flag = None
@@ -172,6 +225,7 @@ class MyWindow:
                     self.row_merge_dict[str(sheet.cell_value(i,int(self.tc_id_t.get())))]=row_to_merge
                     continue
         print(self.row_merge_dict)
+        print("Number of records analysed:%s"%len(self.row_merge_dict))
         return self.row_merge_dict
 
 window=Tk()
